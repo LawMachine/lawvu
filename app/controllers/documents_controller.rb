ff926155ffdfd3585bcfe6c6f606d27399fbd7e3
jony <jony@idifysolutions.com>
2014-08-05 23:04:24 +0530

    commented code to run smooth

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,18 +1,19 @@
 class DocumentsController < ApplicationController
   def index
   end
+  
   def create
   	@doc = Document.new(params[:doc])
   	respond_to do |format| 
   		if @doc.save
   			format.html { redirect_to @doc, notice: 'Document was successfully created.' } 
-  			format.json {
-         data = {id: @doc.id, thumb: view_context.image_tag(@doc.doc.url(:thumb))}
-         render json: data, status: :created, location: @doc
+  			#format.json {
+         #data = {id: @doc.id, thumb: view_context.image_tag(@doc.doc.url(:thumb))}
+         #render json: data, status: :created, location: @doc
   			}
       else
         format.html { render action: "new" }
-        format.json { render json: @doc.errors, status: :unprocessable_entity }
+        #format.json { render json: @doc.errors, status: :unprocessable_entity }
       end
   end
 end
